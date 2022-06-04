bdd2f8457a78608812c79cb8e86c44b9ad3520c3
Muntajim MahiN <85187747+muntajimmahin@users.noreply.github.com>
2022-06-04 23:10:37 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from django.utils.text import slugify
+import csv
+
+a_file = open("titles.txt", "r")
+
+urllist = []
+for line in a_file:
+  stripped_line = line.strip()
+  urllist.append(stripped_line)
+bb = input("Enter The Your Web Site: " )
+for url in urllist:
+    
+    b = (slugify(url))
+    
+    mahin = bb + b
+    print(mahin)
